df35d9f8772c24a000c2eca9100d7306b1e18ce6
PoliPavan <polipavan123@gmail.com>
2024-09-05 22:44:01 +0530

    loops

---
+++ script.js
@@ -0,0 +1,58 @@
+// task1
+
+// const colors = ['red', 'green', 'blue', 'yellow', 'orange'];
+// for (const index in colors) {
+//   console.log(index);
+// }
+
+// task2
+// const animals = ['cat', 'dog', 'rabbit', 'elephant', 'lion'];
+// for (const animal of animals) {
+//   console.log(animal);
+// }
+
+// task3
+
+// const numbers = [2, 4, 6, 8, 10];
+// let sum = 0;
+// let index = 0;
+// while (index < numbers.length) {
+//   sum += numbers[index];
+//   index++;
+// }
+// console.log('Sum of array elements:', sum);
+
+
+// task4
+
+// const numbers = [1, 2, 3, 4, 5];
+// let index = numbers.length - 1;
+// do {
+//   console.log(numbers[index]);
+//   index--;
+// } while (index >= 0);
+
+// task5
+
+
+// const numbers = [3, 6, 9, 12, 15, 18];
+// let sum = 0;
+// let index = 0;
+// do {
+//   if (numbers[index] % 2 === 0) {
+//     sum += numbers[index];
+//   }
+//   index++;
+// } while (index < numbers.length);
+// console.log('Sum of even numbers:', sum);
+
+// task6
+
+const array = ['a', 'b', 'c', 'd', 'e'];
+const reversedArray = [];
+let index = array.length -1;
+while (index >= 0) {
+  console.log(array[index]);
+  index--;
+}
+
